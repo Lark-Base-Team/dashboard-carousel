fc550a84fef0978cfff8c08cf66353b35dac5865
Carter <terrycarter1985@gmail.com>
2024-05-19 14:05:38 +0800

    modify styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Button, Input, Image, Space, Form } from '@douyinfe/semi-ui';
 import { useTheme, useConfig } from "./hooks/index";
 import './App.scss';
 import classnames from 'classnames'
-import { debounce } from 'lodash';
 import { ColorPicker } from "./components/ColorPicker";
 
 
@@ -13,6 +12,7 @@ interface ICarouselConfig {
   color: string;
   fontSize: number;
   showTitle: boolean;
+  text: string;
 }
 
 function App() {
@@ -22,10 +22,9 @@ function App() {
   const [config, setConfig] = useState<ICarouselConfig>({
     color: 'var(--ccm-chart-N700)',
     fontSize: 14,
-    showTitle: true
+    showTitle: true,
+    text: ""
   });
-
-  const [inputValue, setInputValue] = useState('');
 
   const isCreate = dashboard.state === DashboardState.Create
   /** 是否配置模式下 */
@@ -37,7 +36,6 @@ function App() {
     const { customConfig } = res;
     if (customConfig) {
       setConfig(customConfig as any)
-      setInputValue(customConfig.url)
       setTimeout(() => {
         // 预留3s给浏览器进行渲染，3s后告知服务端可以进行截图了
         dashboard.setRendered();
@@ -56,6 +54,8 @@ function App() {
     } as any)
   }
 
+  const { color } = config;
+
   return (
     <main className={classnames({
       'main-config': isConfig,
@@ -63,8 +63,8 @@ function App() {
     })}>
       <div className='content'>
         <div className="marquee">
-          <div className="marquee-content">
-            司机一杯酒，亲人两行泪...        司机一杯酒，亲人两行泪...        司机一杯酒，亲人两行泪...
+          <div style={{ color }} className="marquee-content">
+            {config.text || t("placeholder.text")}
           </div>
         </div>
       </div>
@@ -73,8 +73,13 @@ function App() {
           <div className='config-panel'>
             <Form className='form'>
               <div className='form-item'>
-                <Form.Label className='label'>{t("label.link")}</Form.Label>
-                <Input value={inputValue} placeholder={t("placeholder.link")} onChange={setInputValue}></Input>
+                <Form.Label className='label'>{t("label.text")}</Form.Label>
+                <Input value={config.text} placeholder={t("placeholder.text")} onChange={(v) => {
+                  setConfig({
+                    ...config,
+                    text: v,
+                  })
+                }}></Input>
               </div>
 
               <div className='form-item'>
